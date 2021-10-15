11b2fa0ce84129d9b4ecc480fba3ff61d0569e87
EddieFreedome <eddie.freed@tutanota.com>
2021-10-15 17:05:42 +0200

    excercise 2

---
+++ script-2.js
@@ -0,0 +1,21 @@
+//Crea due array che hanno un numero di elementi diversi.
+//Aggiungi elementi all’array che ha meno elementi
+//fino a quando ne avrà tanti quanti l’altro.
+
+//devo confrontare il numero di valori del primo array con il numero di valori del secondo array
+//se array1 ha piu' elementi di array2, allora pusho numeri nell'array 2, finche' n.array1 sara' uguale al numero di array2
+
+//come posso assegnare una variabile a un numero di valori di un array?
+//posso generare con mathrandom i numeri da inserire casualmente
+
+
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+if (array1.lenght < array2.lenght){
+    array1.push(Math.random() *10)
+} if else (//array2 > 1)
+
+let numValori1 
+let numValori2
+let numDaInserire = MathRandom
