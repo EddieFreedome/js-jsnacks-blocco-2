a9db193729c442558ccbc54f3946b620ea299c19
EddieFreedome <eddie.freed@tutanota.com>
2021-10-15 16:40:17 +0200

    richiesto da flavio

---
+++ script.js
@@ -0,0 +1,12 @@
+//Inserisci un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo.
+// Si potrebbe fare saltando un if o comunque accorpando porzioni di codice.
+//Occhio solo a non saltare troppi passaggi logici che per qualcuno sono ostici.
+
+const insNumero = prompt("Inserisci un numero")
+
+if (insNumero % 2 === 0) {
+    console.log(insNumero)
+} else if (insNumero % 3 === 0) {
+    
+}
